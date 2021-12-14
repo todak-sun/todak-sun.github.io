e8e4d9b0186cea0217c11517e836a494277222f7
todak-sun <tjsdydwn@gmail.com>
2021-12-14 19:13:00 +0900

    깃허브 배포전 커밋

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Todak's Log`,
+    description: `기록`,
+    author: `todak-sun`,
+    siteUrl: `https://todak-sun.github.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
